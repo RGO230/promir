226f8bd22701f18596b662f47d94ea663411cb82
RGO230 <76399317+RGO230@users.noreply.github.com>
2022-12-10 16:51:10 +0300

    working email

--- app/Http/Controllers/StreamApiController.php
+++ app/Http/Controllers/StreamApiController.php
@@ -12,21 +12,22 @@ use Illuminate\Support\Facades\Mail;
 class StreamApiController extends Controller
 {
     public function sendInvite(Request $request){
-        // $streamtoken = Stream::Where('id', $stream->stream_id)->get();
-        $stream = $request->stream_id;
+        $stream = Stream::with('token')->where('id', $request->id)->first();
+        $stream_url= html_entity_decode('Приглашаем вас на прямую трансляцию <a href="'.env('FRONT_HOST').'promir/stream/'.'">'.'promir/streampage'.'?stream_id='.$stream->id.'&'.'streamtoken='.$stream->streamtoken.'</a>');
         $user = User::whereHas('streams',function($q)use($stream){
-            $q->where('stream_id',$stream);
+            $q->where('stream_id',$stream->id);
         })->get();
         foreach($user as $users){
         
+            Mail::send([],[],function ($message) use ($users,$stream_url) {
+                $message->to($users->email, 'Промир')->subject('Вы приглашены на прямую трансляцию');
+                $message->from(env('MAIL_FROM_ADDRESS'), 'Промир');
+                $message->setBody($stream_url, 'text/html');
+                });
         
-        Mail::send('emails.activation',$data, function ($message) use ($users) {
-            $message->to($users->email)->subject('adadsasdsa');
-            $message->from(env('MAIL_FROM_ADDRESS'), 'Чистопар');
-            });
-        }
-        return response($users->email);
             
 
     }
+    return response($user);   
+}
 }
--- resources/views/course/edit.blade.php
+++ resources/views/course/edit.blade.php
@@ -29,7 +29,7 @@
                         <label  class="col-md-4 col-form-label text-md-end">{{ __('Видео') }}</label>
 
                         <div class="col-md-6">
-                            <input type="file" class="form-control" id="customVideoFile" value="{{$course->video}}" name="video" required autofocus>
+                            <input type="text" class="form-control"  value="{{$course->video}}" name="video" required autofocus>
                         </div>
                     </div>
 
